31cdae401b7d5d31b51066251b2e4191f2e8f50e
xylonx <x956200361@gmail.com>
2021-12-19 23:24:55 +0800

    :sparkles: (core) add global core model

---
+++ internal/core/core.go
@@ -0,0 +1,44 @@
+package core
+
+import (
+	"time"
+
+	"github.com/xylonx/icc-core/internal/config"
+	"github.com/xylonx/icc-core/pkg/db"
+	"github.com/xylonx/icc-core/pkg/s3"
+	"github.com/xylonx/zapx"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+var (
+	DB       *gorm.DB
+	S3Client *s3.Client
+)
+
+func Setup() (err error) {
+	DB, err = db.NewPostgreConn(&db.Option{
+		DSN:         config.Config.Database.Postgres.Dsn,
+		MaxOpenConn: int(config.Config.Database.Postgres.MaxOpenConn),
+		MaxIdleConn: int(config.Config.Database.Postgres.MaxIdleConn),
+		MaxLifetime: time.Duration(config.Config.Database.Postgres.MaxLifeSeconds) * time.Second,
+	})
+	if err != nil {
+		zapx.Error("new postgres connection failed", zap.Error(err))
+		return err
+	}
+
+	S3Client, err = s3.NewS3Client(&s3.Option{
+		Endpoint:     config.Config.Storage.S3.Endpoint,
+		AccessID:     config.Config.Storage.S3.AccessId,
+		AccessSecret: config.Config.Storage.S3.AccessSecret,
+		BucketName:   config.Config.Storage.S3.Bucket,
+		Region:       config.Config.Storage.S3.Region,
+	})
+	if err != nil {
+		zapx.Error("new s3 client failed", zap.Error(err))
+		return err
+	}
+
+	return
+}
