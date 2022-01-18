b06ff789aeb3351fb2ae9a874ce7d63fcae1d632
xylonx <x956200361@gmail.com>
2022-01-18 20:23:05 +0800

    :fire: (redis) remove redis related codes

--- internal/core/core.go
+++ internal/core/core.go
@@ -1,11 +1,8 @@
 package core
 
 import (
-	"strconv"
 	"time"
 
-	sess_redis "github.com/gin-contrib/sessions/redis"
-	"github.com/go-redis/redis/v8"
 	"github.com/xylonx/icc-core/internal/config"
 	"github.com/xylonx/icc-core/pkg/db"
 	"github.com/xylonx/icc-core/pkg/s3"
@@ -15,9 +12,8 @@ import (
 )
 
 var (
-	DB                *gorm.DB
-	S3Client          *s3.Client
-	RedisSessionStore sess_redis.Store
+	DB       *gorm.DB
+	S3Client *s3.Client
 )
 
 func Setup() (err error) {
@@ -42,21 +38,6 @@ func Setup() (err error) {
 	})
 	if err != nil {
 		zapx.Error("new s3 client failed", zap.Error(err))
-		return err
-	}
-
-	var redisOpt *redis.Options
-	redisOpt, err = redis.ParseURL(config.Config.Database.Redis.Dsn)
-	if err != nil {
-		zapx.Error("parse redis dsn failed", zap.Error(err))
-		return err
-	}
-	RedisSessionStore, err = sess_redis.NewStoreWithDB(
-		10, redisOpt.Network, redisOpt.Addr, redisOpt.Password,
-		strconv.FormatInt(int64(redisOpt.DB), 10), []byte(config.Config.Application.HttpSessionSecret),
-	)
-	if err != nil {
-		zapx.Error("create redis session store failed", zap.Error(err))
 		return err
 	}
 
