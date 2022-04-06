23c5f9e240555b0ddaff3f7b83aee54965731337
xylonx <x956200361@gmail.com>
2022-04-06 17:33:37 +0800

    :card_file_box: (initsql) change the init table schema sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -2,28 +2,28 @@ create table image(
     created_at timestamp with time zone not null,
     updated_at timestamp with time zone not null,
     deleted_at timestamp with time zone,
-    seq_id BIGSERIAL,
     image_id text not null primary key,
     external_id text,
     md5_sum text not null,
     unique(md5_sum)
 );
-create index idx_image_seq_id on image(seq_id);
 create index idx_image_deleted_at on image(deleted_at);
 create table tag (
     created_at timestamp with time zone not null,
     updated_at timestamp with time zone not null,
     deleted_at timestamp with time zone,
-    tag_name text not null,
-    aliases text [],
-    primary key(tag_name)
+    id integer not null,
+    tag_name_en text,
+    tag_name_cn text,
+    tag_name_jp text,
+    primary key(id)
 );
 create index idx_tag_deleted_at on tag(deleted_at);
 -- using logical constraint to speed up
 create table image_tag_relation(
     image_id text not null,
-    tag_name text not null,
-    primary key (image_id, tag_name)
+    tag_id integer not null,
+    primary key (image_id, tag_id)
 );
 create view rich_image as
 select image.created_at,
@@ -34,11 +34,11 @@ select image.created_at,
     md5_sum,
     array_remove(
         array_agg(
-            DISTINCT tag_name
-            order by tag_name
+            DISTINCT tag_id
+            order by tag_id
         ),
         NULL
-    ) as tags
+    ) as tag_ids
 from image
     left join image_tag_relation on image.image_id = image_tag_relation.image_id
 group by image.image_id;
